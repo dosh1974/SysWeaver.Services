fde1429ef72c6bbfab57de45afcb505361653056
Daniel Hansen <dh@dosh.se>
2025-10-29 14:01:35 +0100

    Added a minimal web server

---
+++ WebServerService/WebServiceServerProgram.cs
@@ -0,0 +1,19 @@
+﻿using SysWeaver.OsServices;
+
+namespace WebServiceServer
+{
+
+    internal class WebServiceServerProgram
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost.Run(new ServiceParams
+            {
+                Name = "WebServiceServer",
+                NeedToRunElevated = true,
+                DisplayName = "SysWeaver - Web Server",
+                Description = "This is a simple \"bare minimum\" web server.",
+            });
+        }
+    }
+}
