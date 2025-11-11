d45b727a273708d0c6f0f4a9f501861ea1fd59a3
SolidEq <dh@solidicon.com>
2025-11-11 10:47:25 +0100

    Added a new Service that could be used to manage services on a server.

---
+++ SwServerManager/ServerManagerProgram.cs
@@ -0,0 +1,16 @@
+﻿using SysWeaver.OsServices;
+
+namespace SysWeaver
+{
+    internal class ServerManagerProgram
+    {
+        static int Main() => ServiceHost.Run(new ServiceParams
+        {
+            Name = "SysWeaver.ServerManager",
+            DisplayName = "SysWeaver  - Server manager",
+            Description = "This is a service that manages other services and folders",
+            NeedToRunElevated = false,
+            RestartOnFail = true,
+        });
+    }
+}
