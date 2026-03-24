d9397fd6f6e2fcc279406b2b9e9c23847804b18f
Daniel Hansen <dh@dosh.se>
2026-03-24 12:07:06 +0100

    Added an example remote proxy server and client.

---
+++ ReverseProxyClient/ReverseProxyClientProgram.cs
@@ -0,0 +1,19 @@
+﻿using SysWeaver.OsServices;
+
+namespace SysWeaver
+{
+    internal class ReverseProxyClientProgram
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost.Run(new ServiceParams
+            {
+                Name = "ReverseProxyClient",
+                NeedToRunElevated = true,
+                DisplayName = "SysWeaver - Reverse proxy client",
+                Description = "An example of a remote proxy client.",
+            });
+
+        }
+    }
+}
---
+++ ReverseProxyServer/ReverseProxyServerProgram.cs
@@ -0,0 +1,19 @@
+﻿using SysWeaver.OsServices;
+
+namespace SysWeaver
+{
+    internal class ReverseProxyServerProgram
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost.Run(new ServiceParams
+            {
+                Name = "ReverseProxyServer",
+                NeedToRunElevated = true,
+                DisplayName = "SysWeaver - Reverse proxy server",
+                Description = "An example of a public facing server that reverse proxy clients connects to.",
+            });
+
+        }
+    }
+}
